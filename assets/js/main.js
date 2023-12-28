29fe98221c373f5b773ea8d7d67549c6d4423155
Victtor777 <victtor.eng.software@gmail.com>
2023-12-28 15:41:36 -0300

    header finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
 
+// verifica se o botão existe e mostra o menu ao clicar.
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// verifica se o botão existe e fecha o menu ao clicar.
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+// remove o menu ao clicar em algum link
+const linkAction = () => {
+  const navMenu = document.querySelector("#nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+// adiciona o evento de click em cada link do menu
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
